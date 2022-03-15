804b52c529d8a4e379b55859ee8c28887a313b86
Ioannis Tourtouras <yiannistourtce@gmail.com>
2022-03-15 13:09:42 +0200

    iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,24 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
+const celebritySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  catchPhrase: {
+    type: String,
+    required: true,
+  },
+  moves: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "pokeMove", required: true },
+  ],
+});
+
+const Celebrity = mongoose.model("celebrity", celebritySchema);
+
+module.exports = Celebrity;
